d230a469bc2025f1001d7bf29619df6d405f66cd
Hana Sabih <hanasabih@gmail.com>
2023-01-17 02:37:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,67 @@
-/* eslint-disable import/extensions */
-
-import Storage, {
-  Card,
-  arrBooks,
+import {
+  form,
+  warningLabel,
+  booksList,
+  addNew,
+  contact,
+  navlist,
+  navadd,
+  navcontact,
   bookTitle,
   bookAuthor,
-} from './modules/storage.js';
-import Books from './modules/books.js';
-import { DateTime } from './modules/luxon.js';
-/* eslint-disable no-unused-vars */
-const bodyCont = document.body;
-const form = document.querySelector('#form');
-const tableContainer = document.querySelector('.cards');
-const warningLabel = document.querySelector('.warningLabel');
-const navbar = document.querySelector('.navbar-nav');
-const booksList = document.querySelector('.books-list');
-const addNew = document.querySelector('.add-new');
-const contact = document.querySelector('.contact');
-const navlist = document.querySelector('.navlist');
-const navadd = document.querySelector('.navadd');
-const navcontact = document.querySelector('.navcontact');
+} from "./modules/variables.js";
+import {
+  addTodStorage,
+  arrBooks,
+  clearInputs,
+  removeBook,
+} from "./modules/storage.js";
+import Books from "./modules/books.js";
+import { displayBook } from "./modules/addBook.js";
+
+import { DateTime } from "./modules/luxon.js";
 
 const d = DateTime.local().toLocaleString(DateTime.DATETIME_FULL);
-
-document.querySelector('.date-display').innerHTML = d;
+document.querySelector(".date-display").innerHTML = d;
 
 let id = arrBooks.length;
-/* eslint-disable no-plusplus */
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (bookTitle.value === '' || bookAuthor.value === '') {
-    warningLabel.classList.remove('d-none');
-  } else {
-    warningLabel.classList.add('d-none');
 
-    const book = new Books(id++, bookTitle.value, bookAuthor.value);
-    // id += 1;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (bookTitle.value === "" || bookAuthor.value === "") {
+    warningLabel.classList.remove("d-none");
+  } else {
+    warningLabel.classList.add("d-none");
+
+    const book = new Books(id, bookTitle.value, bookAuthor.value);
+    id += 1;
 
     arrBooks.push(book);
-    Card.displayBook();
-
-    Card.clearInputs();
-    Card.removeBook();
-
-    Storage.addTodStorage(arrBooks);
+    displayBook();
+    clearInputs();
+    removeBook();
+    addTodStorage(arrBooks);
   }
 });
 
-window.addEventListener('DOMContentLoaded', () => {
-  Card.displayBook();
+window.addEventListener("DOMContentLoaded", () => {
+  displayBook();
   // remove from the dom
-  Card.removeBook();
+  removeBook();
 });
 
 navlist.onclick = () => {
-  booksList.classList.remove('d-none');
-  addNew.classList.add('d-none');
-  contact.classList.add('d-none');
+  booksList.classList.remove("d-none");
+  addNew.classList.add("d-none");
+  contact.classList.add("d-none");
 };
 navadd.onclick = () => {
-  booksList.classList.add('d-none');
-  addNew.classList.remove('d-none');
-  contact.classList.add('d-none');
+  booksList.classList.add("d-none");
+  addNew.classList.remove("d-none");
+  contact.classList.add("d-none");
 };
 navcontact.onclick = () => {
-  booksList.classList.add('d-none');
-  addNew.classList.add('d-none');
-  contact.classList.remove('d-none');
+  booksList.classList.add("d-none");
+  addNew.classList.add("d-none");
+  contact.classList.remove("d-none");
 };
